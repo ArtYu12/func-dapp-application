7e2e8447dc090ccfb60b42536ed2e2026d4b4eff
Matvey <matvey@MacBook-Pro-Matvej.local>
2024-12-03 23:01:05 +0500

    gh-config

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 
 createRoot(document.getElementById('root')!).render(
-    <TonConnectUIProvider  manifestUrl='https://ArtYu12.github.io/func-dapp-application/tonconnect-manifest.json'>
+    <TonConnectUIProvider  manifestUrl='https://artyu12.github.io/func-dapp-application/tonconnect-manifest.json'>
         <App />
     </TonConnectUIProvider>
 )
